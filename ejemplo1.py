ac46b170aa92f083961cabd70fb2669975f18187
arielAlanCruz <acruzcusi@uade.edu.ar>
2024-08-21 21:23:14 -0300

    Nuevos cambios hechos

--- ejemplo1.py
+++ ejemplo1.py
@@ -1,1 +1,10 @@
 print("mensaje de Ariel")
+
+#pasaje de variables
+a=int(input("Ingrese valor de a: "))
+b=int(input("Ingrese valor de b: "))
+
+a,b=b,a
+print("Traspaso...")
+print("valor de a:",a)
+print("valor de b:",b)
